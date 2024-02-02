991dd07d744615259447baee88bd446d6c00904c
gevin <gmadharh@uoguelph.ca>
2024-02-02 15:16:26 -0500

    removed boilerplate, added my links and simple WIP description

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1 class="mb-4 text-3xl">Hey, welcome to my website.</h1>
+    <p>
+      There's nothing here right now, its currently a work in progress. But
+      check back later for some cool stuff. 😎
+    </p>
+
+    <br />
+
+    <p>In the meantime, check me out here:</p>
+    <ul class="pl-5 list-disc">
+      <li>
+        <a
+          class="text-blue-400"
+          href="https://www.linkedin.com/in/gevinm/"
+          target="_blank"
+          >LinkedIn</a
+        >
+      </li>
+      <li>
+        <a
+          class="text-blue-400"
+          href="https://github.com/gmadharh"
+          target="_blank"
+          >GitHub</a
+        >
+      </li>
+    </ul>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup lang="ts"></script>
