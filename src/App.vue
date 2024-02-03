d39c39d1f75c654b665c055068bf6f1db068567b
gevin <gmadharh@uoguelph.ca>
2024-02-03 18:23:46 -0500

    fixed importing icon

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,5 @@
 <template>
   <div class="flex flex-col items-center">
-    <!-- 
-    <h1 class="mb-4 text-3xl">Hey, welcome to my website.</h1>
-    <p>
-      There's nothing here right now, its currently a work in progress. But
-      check back later for some cool stuff. 😎
-    </p>
-
-    <br />
-
-    <p>In the meantime, check me out here:</p>
-    <ul class="pl-5 list-disc">
-      <li>
-        <a
-          class="text-blue-400"
-          href="https://www.linkedin.com/in/gevinm/"
-          target="_blank"
-          >LinkedIn</a
-        >
-      </li>
-      <li>
-        <a
-          class="text-blue-400"
-          href="https://github.com/gmadharh"
-          target="_blank"
-          >GitHub</a
-        >
-      </li>
-    </ul>
-  -->
     <SplashIntro />
     <ProjectCard
       v-for="project in projects"
@@ -43,12 +14,13 @@
 <script setup lang="ts">
 import SplashIntro from './components/SplashIntro.vue'
 import ProjectCard from './components/ProjectCard.vue'
+import spotifyIcon from './assets/spotify-song-extractor.png'
 
 const projects = [
   {
     name: 'Spotify Song Extractor',
     link: 'https://chromewebstore.google.com/detail/spotify-song-extractor/mnaooedpbbimbgcjlfebmkomloakbeag?hl=en&authuser=2',
-    icon: '../assets/spotify-song-extractor.png',
+    icon: spotifyIcon,
   },
 ]
 </script>
