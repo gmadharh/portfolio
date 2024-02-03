32c8ca6a7ee62dc4f0112ca47c09942dbffeaae2
gevin <gmadharh@uoguelph.ca>
2024-02-03 17:40:25 -0500

    changed button to anchor so it can go to project

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,7 +1,8 @@
 <template>
-  <button
+  <a
+    href="https://chromewebstore.google.com/"
     class="flex items-center justify-center flex-auto w-1/3 transition duration-300 ease-in-out delay-150 rounded-full bg-rose-900 hover:-translate-y-1 hover:scale-110 hover:bg-rose-500 border-12 h-14"
-    type="button"
+    target="_blank"
   >
     <img
       src="../assets/spotify-song-extractor.png"
@@ -9,7 +10,7 @@
       class="w-6 h-6 mr-2"
     />
     <span>Spotify Song Extractor</span>
-  </button>
+  </a>
 </template>
 
 <script lang="ts">
