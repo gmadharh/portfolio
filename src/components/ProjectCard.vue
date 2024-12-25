1a4745cf595967e8b4719643e143cad65ed1ff34
gmadharh <gevinmadharha@gmail.com>
2024-12-25 12:16:39 -0500

    Removed color for project pill

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,7 +1,7 @@
 <template>
   <a
     :href="link"
-    class="flex items-center justify-center flex-auto w-10/12 m-2 transition duration-300 ease-in-out delay-150 rounded-full 2xl:w-1/4 md:w-1/2 bg-rose-900 hover:-translate-y-1 hover:scale-110 hover:bg-rose-500 border-12 h-14"
+    class="flex items-center justify-center flex-auto w-10/12 m-2 transition duration-300 ease-in-out delay-150 rounded-full 2xl:w-1/4 md:w-1/2 hover:-translate-y-1 hover:scale-110 border-12 h-14"
     target="_blank"
   >
     <img :src="icon" alt="Project Icon" class="w-6 h-6 mr-2" />
