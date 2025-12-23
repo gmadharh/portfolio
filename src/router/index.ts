7612409ae42cec46fc08a9b0c00d39e8b25e80c1
Gevin Madharha <gevinmadharha@gmail.com>
2025-12-23 18:53:00 -0500

    remove Soli Deo Tech

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import SoliDeoTech from '../views/SoliDeoTech.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
-      path: '/',
-      name: 'Home',
-      component: Home
+      path: "/",
+      name: "Home",
+      component: Home,
     },
-    {
-      path: '/solideotech',
-      name: 'SoliDeoTech',
-      component: SoliDeoTech
-    }
-  ]
-})
+  ],
+});
 
-export default router
-
+export default router;
